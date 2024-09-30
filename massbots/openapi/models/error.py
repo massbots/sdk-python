420ae7c8d7eb440821707da2a8c4e03cea276b2b
Artem Kudrin <akudrin2002@gmail.com>
2024-09-30 21:07:18 +0300

    error: removed deprecated annotations

--- massbots/openapi/models/error.py
+++ massbots/openapi/models/error.py
@@ -1,16 +1,8 @@
 from typing import (
     Any,
-    Dict,
     Type,
     TypeVar,
-    Tuple,
-    Optional,
-    BinaryIO,
-    TextIO,
-    TYPE_CHECKING,
 )
-
-from typing import List
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
@@ -27,12 +19,12 @@ class Error:
     """
 
     error: str
-    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
-    def to_dict(self) -> Dict[str, Any]:
+    def to_dict(self) -> dict[str, Any]:
         error = self.error
 
-        field_dict: Dict[str, Any] = {}
+        field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update(
             {
@@ -43,7 +35,7 @@ class Error:
         return field_dict
 
     @classmethod
-    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+    def from_dict(cls: Type[T], src_dict: dict[str, Any]) -> T:
         d = src_dict.copy()
         error = d.pop("error")
 
@@ -55,7 +47,7 @@ class Error:
         return error
 
     @property
-    def additional_keys(self) -> List[str]:
+    def additional_keys(self) -> list[str]:
         return list(self.additional_properties.keys())
 
     def __getitem__(self, key: str) -> Any:
